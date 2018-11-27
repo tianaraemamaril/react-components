e0e651220be0c7f196bc3321a4ab1eb964854552
Tiana Rae Mamaril <tianaraetran@gmail.com>
2018-11-26 17:04:04 -0800

    Have partially working code.

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
 // TODO
+// TODO
+// var GroceryList = () => (
+//   <ul>
+//     <li>green tea</li>
+//     <li>strawberries</li>
+//   </ul> 
+
+// );
+
+// var GreenTea = () => (
+//   <li>GreenTea</li>
+// );
+
+// var Strawberries = () => (
+//   <li>Strawberries</li>
+// );
+
+
+// var GroceryList = () => (
+//   <ul>
+//     <GreenTea />
+//     <Strawberries />
+//   </ul>
+// );    
+
+ 
+
+
+var GroceryList = (props) => (
+  <ul>
+    <li>
+      {props.items[0]}
+    </li>
+    <li>
+      {props.items[1]}
+    </li>
+    <GroceryListItem item = {props.items[0]}/>
+  </ul>
+
+
+  
+);
+
+var GroceryListItem = (props) => (
+  <li>
+    {props.item}
+  </li>
+);
+
+
+
+
+ReactDOM.render(
+    <GroceryList items = {['GreenTea', 'Strawberries']} />, 
+    document.getElementById('app')
+)
+
